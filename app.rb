294c40c678084fc4e56e093f24df13339ca5f057
parmeso6 <sophie.marie.parmentier@gmail.com>
2015-08-19 10:33:53 +0200

    essai response avec binding.pry

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
 require 'unirest'
+require 'pry'
+require 'json'
 
 get "/" do
+	response = Unirest.get "https://zilyo.p.mashape.com/search?isinstantbook=true&nelatitude=22.37&nelongitude=-154.48000000000002&provider=airbnb%2Chousetrip&swlatitude=18.55&swlongitude=-160.52999999999997",
+  headers:{
+    "X-Mashape-Key" => "dtFSGSEqxtmshQloyQuNPHBymLNSp1aN6M1jsnE1XxVJqxvxKE",
+    "Accept" => "application/json"
+  }
+  binding.pry
+  @response = JSON.parse(response)
   erb :index
 end
